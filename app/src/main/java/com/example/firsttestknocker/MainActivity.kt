db18c30c1f5c58655503fcd5b8d4d5660ef3cd52
Ryan <Ryan.granet@epitech.eu>
2019-04-09 15:57:18 +0200

    Add permission to access contact

--- app/src/main/java/com/example/firsttestknocker/MainActivity.kt
+++ app/src/main/java/com/example/firsttestknocker/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.firsttestknocker
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
@@ -55,6 +58,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS),1)
+        }
 
         // on init WorkerThread
         main_mDbWorkerThread = DbWorkerThread("dbWorkerThread")
