126b95afa9a3ad319cccbd30ff72bc638bd8ec75
Kenzy Suon <suonkenzy@hotmail.com>
2019-04-09 15:50:53 +0200

    Update AddNewContact and EditContact, add the validate Button

--- app/src/main/java/com/example/firsttestknocker/AddNewContactActivity.java
+++ app/src/main/java/com/example/firsttestknocker/AddNewContactActivity.java
@@ -8,8 +8,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddNewContactActivity extends AppCompatActivity {
+
+    private TextView add_new_contact_FirstName;
+    private TextView add_new_contact_LastName;
+    private TextView add_new_contact_PhoneNumber;
+    private RoundedImageView add_new_contact_RoundedImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +31,30 @@ public class AddNewContactActivity extends AppCompatActivity {
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_left_arrow);
         actionbar.setTitle("Ajouter un nouveau contact");
+
+        // Find View By Id
+        add_new_contact_FirstName = findViewById(R.id.add_new_contact_first_name_id);
+        add_new_contact_LastName = findViewById(R.id.add_new_contact_last_name_id);
+        add_new_contact_PhoneNumber = findViewById(R.id.add_new_contact_phone_number_id);
+        add_new_contact_RoundedImageView = findViewById(R.id.add_new_contact_rounded_image_view_id);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                Intent loginIntent = new Intent(AddNewContactActivity.this, MainActivity.class);
-                startActivity(loginIntent);
-                finish();
-                return true;
+                onBackPressed();
+            case R.id.nav_validate:
+                if (add_new_contact_FirstName.getText().toString().isEmpty() && add_new_contact_LastName.getText().toString().isEmpty() ) {
+                    Toast.makeText(this, "Les champs nom et prénom ne peuvent pas être vide !", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Query
+                    Intent intent = new Intent(AddNewContactActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
         }
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/example/firsttestknocker/EditContactActivity.java
+++ app/src/main/java/com/example/firsttestknocker/EditContactActivity.java
@@ -8,38 +8,71 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class EditContactActivity extends AppCompatActivity {
+
+    private TextView edit_contact_FirstName;
+    private TextView edit_contact_LastName;
+    private TextView edit_contact_PhoneNumber;
+    private RoundedImageView edit_contact_RoundedImageView;
+    private String edit_contact_first_name;
+    private String edit_contact_last_name;
+    private String edit_contact_phone_number;
+    private int edit_contact_rounded_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_contact);
 
+        // Create the Intent, and get the data from the GridView
+        Intent intent = getIntent();
+        edit_contact_first_name = intent.getStringExtra("ContactFirstName");
+        edit_contact_last_name = intent.getStringExtra("ContactLastName");
+        edit_contact_phone_number = intent.getStringExtra("ContactPhoneNumber");
+        edit_contact_rounded_image = intent.getIntExtra("ContactImage", 1);
+
         // Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_left_arrow);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_cross);
         actionbar.setTitle("Editer le contact");
 
-        // Create the Intent, and get the data from the GridView
-        Intent intent = getIntent();
-        String contact_details_first_name = intent.getStringExtra("ContactFirstName");
-        String contact_details_last_name = intent.getStringExtra("ContactLastName");
-        String contact_details_phone_number = intent.getStringExtra("ContactPhoneNumber");
-        int contactImage = intent.getIntExtra("ContactImage", 1);
+        // Find View By Id
+        edit_contact_FirstName = findViewById(R.id.edit_contact_first_name_id);
+        edit_contact_LastName = findViewById(R.id.edit_contact_last_name_id);
+        edit_contact_PhoneNumber = findViewById(R.id.edit_contact_phone_number_id);
+        edit_contact_RoundedImageView = findViewById(R.id.edit_contact_rounded_image_view_id);
+
+        // Set Resources from MainActivity to ContactDetailsActivity
+        edit_contact_FirstName.setText(edit_contact_first_name);
+        edit_contact_LastName.setText(edit_contact_last_name);
+        edit_contact_PhoneNumber.setText(edit_contact_phone_number);
+        edit_contact_RoundedImageView.setImageResource(edit_contact_rounded_image);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                Intent loginIntent = new Intent(EditContactActivity.this, ContactDetailsActivity.class);
-                startActivity(loginIntent);
+                onBackPressed();
+            case R.id.nav_validate:
+                // Update
+                Intent intent = new Intent(EditContactActivity.this, ContactDetailsActivity.class);
+
+                intent.putExtra("ContactFirstName", edit_contact_first_name);
+                intent.putExtra("ContactLastName", edit_contact_last_name);
+                intent.putExtra("ContactPhoneNumber", edit_contact_phone_number);
+                intent.putExtra("ContactImage", edit_contact_rounded_image);
+
+                startActivity(intent);
                 finish();
-                return true;
         }
         return super.onOptionsItemSelected(item);
     }
