c995f323a67a2afdab699e889c16d893a1f54053
Kenzy Suon <suonkenzy@hotmail.com>
2019-04-10 16:07:18 +0200

    Add UI for Hub, CustomPopup and update or add files .xml

---
+++ app/src/main/java/com/example/firsttestknocker/ChatActivity.java
@@ -0,0 +1,18 @@
+package com.example.firsttestknocker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+//        EditText testEditText = findViewById(R.id.testEditText);
+    }
+}
