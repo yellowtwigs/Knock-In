2e67878bf8518055ca6729f7cb84400de1a81e1c
Kenzy Suon <suonkenzy@hotmail.com>
2019-04-11 18:17:21 +0200

    Add link to Whatsapp

--- app/src/main/java/com/example/firsttestknocker/ChatActivity.java
+++ app/src/main/java/com/example/firsttestknocker/ChatActivity.java
@@ -18,6 +18,7 @@ public class ChatActivity extends AppCompatActivity {
 
     Integer REQUEST_CAMERA = 1, SELECT_FILE = 0;
     ImageView messenger;
+    ImageView whatsapp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,61 +26,21 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
         messenger = findViewById(R.id.messenger);
+        whatsapp = findViewById(R.id.whatsapp);
 
         messenger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SelectImage();
-//                gotToFacebookPage("");
+                gotToFacebookPage("");
             }
         });
-    }
 
-    private void SelectImage() {
-
-        final CharSequence[] items = {"Camera", "Gallery", "Cancel"};
-//            ActionBar.DisplayOptions[]
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(ChatActivity.this);
-        builder.setTitle("Add Image");
-        builder.setItems(items, new DialogInterface.OnClickListener() {
+        whatsapp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int i) {
-                if (items[i].equals("Camera")) {
-
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(intent, REQUEST_CAMERA);
-
-                } else if (items[i].equals("Gallery")) {
-
-                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    intent.setType("image/*");
-                    startActivityForResult(intent.createChooser(intent, "Select File"), SELECT_FILE);
-
-                } else if (items[i].equals("Cancel")) {
-                    dialog.dismiss();
-                }
+            public void onClick(View v) {
+                gotToFacebookPage("");
             }
         });
-        builder.show();
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == REQUEST_CAMERA) {
-
-                Bundle bundle = data.getExtras();
-                final Bitmap bitmap = (Bitmap) bundle.get("data");
-                messenger.setImageBitmap(bitmap);
-
-            } else if (requestCode == SELECT_FILE) {
-                Uri selectedImageUri = data.getData();
-                messenger.setImageURI(selectedImageUri);
-            }
-        }
     }
 
     private void gotToFacebookPage(String id) {
