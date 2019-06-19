8fc4cb01720b3cac5088fc99fc6983830c2f6cc6
Kenzy Suon <suonkenzy@hotmail.com>
2019-06-19 15:05:48 +0200

    Edit SelectContactAdapter

--- app/src/main/java/com/example/knocker/controller/activity/firstLaunch/SelectContactAdapter.java
+++ app/src/main/java/com/example/knocker/controller/activity/firstLaunch/SelectContactAdapter.java
@@ -222,23 +222,18 @@ public class SelectContactAdapter extends BaseAdapter {
         return contactList;
     }
 
-    private int randomDefaultImage(int avatarId, String createOrGet) {
+    public int randomDefaultImage(int avatarId, String createOrGet) {
         if (createOrGet.equals("Create")) {
-            return new Random().nextInt(6);
+            return new Random().nextInt(7);
         } else if (createOrGet.equals("Get")) {
             switch(avatarId) {
-                case 0:
-                    return R.drawable.ic_user_purple;
-                case 1:
-                    return R.drawable.ic_user_blue;
-                case 2:
-                    return R.drawable.ic_user_orange;
-                case 3:
-                    return R.drawable.ic_user_green;
-                case 4:
-                    return R.drawable.ic_user_om;
-                case 5:
-                    return R.drawable.ic_user_pink;
+                case 0: return R.drawable.ic_user_purple;
+                case 1: return R.drawable.ic_user_blue;
+                case 2: return R.drawable.ic_user_brown;
+                case 3: return R.drawable.ic_user_green;
+                case 4: return R.drawable.ic_user_om;
+                case 5: return R.drawable.ic_user_orange;
+                case 6: return R.drawable.ic_user_pink;
             }
         }
         return -1;
