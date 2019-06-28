9c3c44270ecd8e0fa19c4f584bd2bd295f0111fc
florian Striebel <florian.striebel@live.fr>
2019-06-28 09:17:46 +0200

    add new restriction NotificationListener

--- app/src/main/java/com/example/knocker/controller/NotificationListener.kt
+++ app/src/main/java/com/example/knocker/controller/NotificationListener.kt
@@ -224,10 +224,11 @@ class NotificationListener : NotificationListenerService() {
 
 
     fun appNotifiable(sbp: StatusBarParcelable): Boolean {
+        val regexNewMessage = "[0-9]+ nouveaux messages".toRegex()
         return sbp.statusBarNotificationInfo["android.title"] != "Chat heads active" &&
                 sbp.statusBarNotificationInfo["android.title"] != "Messenger" &&
                 sbp.statusBarNotificationInfo["android.title"] != "Bulles de discussion activées" &&
-                convertPackageToString(sbp.appNotifier) != ""
+                convertPackageToString(sbp.appNotifier) != "" && !sbp.statusBarNotificationInfo["android.title"].toString().matches(regexNewMessage)
     }
 
     private fun convertPackageToString(packageName: String): String {
