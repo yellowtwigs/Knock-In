2249d52d63d55900dd05b29813f454b78dda8b57
Kenzy <suonkenzy@hotmail.com>
2019-09-02 15:52:24 +0200

    Work on Sleep Screen Alarm without bugs

---
+++ app/src/main/java/com/yellowtwigs/knocker/controller/NotificationAlarmRecyclerViewAdapter.kt
@@ -0,0 +1,190 @@
+package com.yellowtwigs.knocker.controller
+
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.PorterDuff
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.util.Base64
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.TranslateAnimation
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+import com.yellowtwigs.knocker.R
+import com.yellowtwigs.knocker.controller.activity.EditContactActivity
+import com.yellowtwigs.knocker.controller.activity.MainActivity
+import com.yellowtwigs.knocker.controller.activity.NotificationAlarmActivity
+import com.yellowtwigs.knocker.controller.activity.group.GroupActivity
+import com.yellowtwigs.knocker.model.*
+import com.yellowtwigs.knocker.model.ModelDB.ContactDB
+import com.yellowtwigs.knocker.model.ModelDB.ContactWithAllInformation
+import com.yellowtwigs.knocker.model.ModelDB.GroupDB
+import com.yellowtwigs.knocker.model.ModelDB.NotificationDB
+
+import java.util.ArrayList
+import java.util.Objects
+
+import java.sql.DriverManager.println
+
+/**
+ * La Classe qui permet de remplir la RecyclerView avec les bon éléments
+ *
+ * @author Florian Striebel, Kenzy Suon, Ryan Granet
+ */
+class NotificationAlarmRecyclerViewAdapter(private val context: Context, private var notification_alarm_ListOfNotification: MutableList<StatusBarParcelable>, private var nbOfMessages: Int) : RecyclerView.Adapter<NotificationAlarmRecyclerViewAdapter.NotificationAlarmViewHolder>() {
+    private var notification_alarm_ContactsDatabase: ContactsRoomDatabase? = null
+    private var view: View? = null
+
+    private val FACEBOOK_PACKAGE = "com.facebook.katana"
+    private val MESSENGER_PACKAGE = "com.facebook.orca"
+    private val WHATSAPP_SERVICE = "com.whatsapp"
+    private val GMAIL_PACKAGE = "com.google.android.gm"
+    private val MESSAGE_PACKAGE = "com.google.android.apps.messaging"
+    private val MESSAGE_SAMSUNG_PACKAGE = "com.samsung.android.messaging"
+
+    fun getItem(position: Int): StatusBarParcelable {
+        return notification_alarm_ListOfNotification[position]
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationAlarmViewHolder {
+        view = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_notification_alarm_layout, parent, false)
+
+        return NotificationAlarmViewHolder(view!!)
+    }
+
+    override fun onBindViewHolder(holder: NotificationAlarmViewHolder, position: Int) {
+        val notification = getItem(position)
+        val gestionnaireContacts = ContactManager(context)
+
+        val contact = gestionnaireContacts.getContact(notification_alarm_ListOfNotification[position].statusBarNotificationInfo["android.title"] as String)
+
+        notification_alarm_ContactsDatabase = ContactsRoomDatabase.getDatabase(context)
+
+        if (context is NotificationAlarmActivity) {
+
+            when (notification.appNotifier!!) {
+                "com.whatsapp" -> {
+                    holder.item_recycler_notification_alarm_ImageView.setImageResource(R.drawable.ic_circular_whatsapp)
+
+                    if (nbOfMessages == 1) {
+                        holder.item_recycler_notification_alarm_Text.text = context.getString(R.string.notification_alarm_message_received)
+                    } else {
+                        holder.item_recycler_notification_alarm_Text.text = nbOfMessages.toString() + " " + context.getString(R.string.notification_alarm_messages_received)
+                    }
+                }
+
+                "com.google.android.gm" -> holder.item_recycler_notification_alarm_ImageView.setImageResource(R.drawable.ic_gmail)
+
+                "com.google.android.apps.messaging",
+                "com.android.mms",
+                "com.samsung.android.messaging" -> {
+                    holder.item_recycler_notification_alarm_ImageView.setImageResource(R.drawable.ic_sms)
+
+                    if (nbOfMessages == 1) {
+                        holder.item_recycler_notification_alarm_Text.text = context.getString(R.string.notification_alarm_message_received)
+                    } else {
+                        holder.item_recycler_notification_alarm_Text.text = nbOfMessages.toString() + " " + context.getString(R.string.notification_alarm_messages_received)
+                    }
+                }
+            }
+        }
+
+        holder.item_recycler_notification_alarm_Layout.setOnClickListener {
+            when (notification.appNotifier!!) {
+                "com.whatsapp" -> {
+                    if (contact != null) {
+                        openWhatsapp(contact.getFirstPhoneNumber())
+                    } else {
+                        openWhatsapp(notification.statusBarNotificationInfo["android.title"] as String)
+                    }
+                }
+
+                "com.google.android.gm" -> context.startActivity(Intent())
+
+                "com.google.android.apps.messaging",
+                "com.android.mms",
+                "com.samsung.android.messaging" -> {
+                    if (contact != null) {
+                        openSms(contact.getFirstPhoneNumber())
+                    } else {
+                        openSms(notification.statusBarNotificationInfo["android.title"] as String)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun getItemId(position: Int): Long {
+        return notification_alarm_ListOfNotification.size.toLong()
+    }
+
+    private fun openSms(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("sms", phoneNumber, null))
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+        context.startActivity(intent)
+    }
+
+    private fun openWhatsapp(phoneNumber: String) {
+
+        val intent = Intent(Intent.ACTION_VIEW)
+        val message = "phone=" + converter06To33(phoneNumber)
+        intent.data = Uri.parse("http://api.whatsapp.com/send?phone=$message")
+
+        context.startActivity(intent)
+    }
+
+    private fun converter06To33(phoneNumber: String): String {
+        return if (phoneNumber[0] == '0') {
+            "+33 $phoneNumber"
+        } else phoneNumber
+    }
+
+    private fun convertPackageToString(packageName: String): String {
+        if (packageName == FACEBOOK_PACKAGE) {
+            return "Facebook"
+        } else if (packageName == MESSENGER_PACKAGE) {
+            return "Messenger"
+        } else if (packageName == WHATSAPP_SERVICE) {
+            return "WhatsApp"
+        } else if (packageName == GMAIL_PACKAGE) {
+            return "Gmail"
+        } else if (packageName == MESSAGE_PACKAGE || packageName ==
+                MESSAGE_SAMSUNG_PACKAGE) {
+            return "Message"
+        }
+        return ""
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+     */
+    override fun getItemCount(): Int {
+        return notification_alarm_ListOfNotification.size
+    }
+
+    inner class NotificationAlarmViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var item_recycler_notification_alarm_Layout: RelativeLayout = view.findViewById(R.id.item_recycler_notification_alarm_layout)
+        var item_recycler_notification_alarm_ImageView: AppCompatImageView = view.findViewById(R.id.item_recycler_notification_alarm_image)
+        var item_recycler_notification_alarm_Text: TextView = view.findViewById(R.id.item_recycler_notification_alarm_text)
+    }
+}
--- app/src/main/java/com/yellowtwigs/knocker/controller/activity/NotificationAlarmActivity.kt
+++ app/src/main/java/com/yellowtwigs/knocker/controller/activity/NotificationAlarmActivity.kt
@@ -9,42 +9,50 @@ import android.view.WindowManager
 import android.widget.TextView
 import com.yellowtwigs.knocker.model.StatusBarParcelable
 import android.annotation.SuppressLint
+import android.content.SharedPreferences
 import android.os.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
+import com.yellowtwigs.knocker.controller.NotificationAlarmRecyclerViewAdapter
+import com.yellowtwigs.knocker.model.ModelDB.NotificationDB
 
 
 class NotificationAlarmActivity : AppCompatActivity() {
 
-    private var notification_Alarm_Sender_TextView: TextView? = null
-    private var notification_Alarm_Content_TextView: TextView? = null
-    private var notification_Alarm_Button_close: MaterialButton? = null
-    private var notification_Alarm_Button_response: MaterialButton? = null
+    private var notification_Alarm_Button_ShutDown: MaterialButton? = null
+
+    private var notification_alarm_RecyclerView: RecyclerView? = null
+    private var notification_alarm_RecyclerViewAdapter: NotificationAlarmRecyclerViewAdapter? = null
+    private var sharedThemePreferences: SharedPreferences? = null
+
+    private var cpt : Int? = null
+
     private var isOpen = true
 
 //    private var sound : SoundPool
-
-    private var notification_alarm_sender: String = ""
-    private var notification_alarm_content: String = ""
-
 
     @SuppressLint("InvalidWakeLockTag")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification_alarm)
 
-        notification_Alarm_Content_TextView = findViewById(R.id.notification_alarm_description)
-        notification_Alarm_Sender_TextView = findViewById(R.id.notification_alarm_contact)
-        notification_Alarm_Button_close = findViewById(R.id.notification_alarm_floating_button_close)
-        notification_Alarm_Button_response = findViewById(R.id.notification_alarm_floating_button_openMessage)
+        sharedThemePreferences = getSharedPreferences("nbOfMessages", Context.MODE_PRIVATE)
+        cpt = sharedThemePreferences!!.getInt("nbOfMessages", 1)
 
-        val sbp = intent.extras.get("notification") as StatusBarParcelable
-        notification_alarm_sender = sbp.statusBarNotificationInfo.get("android.title") as String
-        notification_alarm_content = sbp.statusBarNotificationInfo.get("android.text") as String
+        notification_Alarm_Button_ShutDown = findViewById(R.id.notification_alarm_shut_down)
+        notification_alarm_RecyclerView = findViewById(R.id.notification_alarm_recycler_view)
 
-        notification_Alarm_Content_TextView!!.setText(notification_alarm_content)
-        notification_Alarm_Sender_TextView!!.setText(notification_alarm_sender)
+        val sbp = intent.extras!!.get("notification") as StatusBarParcelable
+
+        val notification_alarm_ListOfNotification: ArrayList<StatusBarParcelable> = ArrayList()
+        notification_alarm_ListOfNotification.add(sbp)
+
+        notification_alarm_RecyclerViewAdapter = NotificationAlarmRecyclerViewAdapter(this, notification_alarm_ListOfNotification, cpt!!)
+        notification_alarm_RecyclerView!!.layoutManager = LinearLayoutManager(applicationContext)
+        notification_alarm_RecyclerView!!.adapter = notification_alarm_RecyclerViewAdapter
+
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        println("sbp" + sbp.TAG)
         println("I'm into alarm")
 
         if (Build.VERSION.SDK_INT >= 27) {
@@ -59,51 +67,49 @@ class NotificationAlarmActivity : AppCompatActivity() {
                     or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
                     or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON)
         }
+
         //region sound + vibration
         /* sound.start()
          sound.isLooping=true*/
 
-        val vibration = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val thread = Thread {
-                val timeWhenLaunch = System.currentTimeMillis()
-                while (isOpen && System.currentTimeMillis() - timeWhenLaunch < 20 * 1000) {
-                    vibration.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
-                    Thread.sleep(1000)
-                }
-                    finish()
+//        val vibration = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val thread = Thread {
+//                val timeWhenLaunch = System.currentTimeMillis()
+//                while (isOpen && System.currentTimeMillis() - timeWhenLaunch < 20 * 1000) {
+//                    vibration.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
+//                    Thread.sleep(1000)
+//                }
+//                finish()
+//
+//            }
+//            thread.start()
+//        } else {
+//            //deprecated in API 26
+//            vibration.vibrate(500)
+//        }
 
-            }
-            thread.start()
-        } else {
-            //deprecated in API 26
-            vibration.vibrate(500)
-        }
         //endregion
-        notification_Alarm_Button_close!!.setOnClickListener {
+
+        notification_Alarm_Button_ShutDown!!.setOnClickListener {
             this.finish()
             isOpen = false
             // sound.stop()
         }
+    }
 
-        notification_Alarm_Button_response!!.setOnClickListener {
-            //val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
-            //val fullWakeLock = powerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK or PowerManager.FULL_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP, "Loneworker - FULL WAKE LOCK")
-           // val partialWakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "PowerManager.PARTIAL_WAKE_LOCK")
+    override fun onStop() {
+        super.onStop()
 
-            println("app notifier " + sbp.appNotifier)
-            val intent = packageManager.getLaunchIntentForPackage(sbp.appNotifier)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            startActivity(intent)
-            /*if (fullWakeLock.isHeld()) {
-                fullWakeLock.release()
-            }
-            if (partialWakeLock.isHeld()) {
-                partialWakeLock.release()
-            }*/
-            isOpen = false
-            finish()
-            // sound.stop()
-        }
+        cpt = cpt!!.plus(1)
+        finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        val edit: SharedPreferences.Editor = sharedThemePreferences!!.edit()
+        edit.putInt("nbOfMessages", 1)
+        edit.apply()
     }
 }
